5ab2d636eb32aa1a937660826d93f7ba97b9bcae
Jbang Macbook <jrbangit03@Jbangs-MacBook-Air.local>
2024-08-31 21:05:07 +0900

    change db connect addr

--- src/database.rs
+++ src/database.rs
@@ -2,6 +2,6 @@ use sqlx::{Pool, MySql, MySqlPool};
 
 
 pub async fn database_connect() -> Result<MySqlPool, sqlx::Error> {
-    MySqlPool::connect("mysql://root:root@localhost:3306/simple_restaurant").await
+    MySqlPool::connect("mysql://root:root@0.0.0.0:3306/simple_restaurant").await
 }
 
